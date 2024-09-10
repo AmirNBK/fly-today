707133dbbecdc8b68cc4b18838f3f775c18d1f1c
AmirHossein Norbakhsh <amircr7h@icloud.com>
2024-09-10 18:59:56 +0330

    the departure and arrival  cities are not shown in url

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
   return (
     <main className={`${IranSans.className} flex flex-row p-6 w-10/12 gap-11 mx-auto mt-12`}>
-    <Link className='mx-auto' href={'/flight/search'}>
+    <Link className='mx-auto' href={'/flight/search?departure=thr,1&arrival=ist'}>
     <Button className='bg-[#ff7913]'>
       مشاهده  نتایج جستجو
     </Button>
--- components/PaginatorComponent/PaginatorComponent.tsx
+++ components/PaginatorComponent/PaginatorComponent.tsx
@@ -2,11 +2,11 @@
 import React, { useState } from 'react';
 import { Paginator, PaginatorPageChangeEvent } from 'primereact/paginator';
 import { FlightData } from '@/types/types';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 const PaginatorComponent = ({ data }: { data: FlightData }) => {
-
     const router = useRouter();
+    const searchParams = useSearchParams(); // Access the current query parameters
     const [first, setFirst] = useState<number>(0);
     const [rows, setRows] = useState<number>(7);
 
@@ -14,7 +14,15 @@ const PaginatorComponent = ({ data }: { data: FlightData }) => {
         const newPage = (event.first / event.rows) + 1;
         setFirst(event.first);
         setRows(event.rows);
-        router.push(`/flight/search?page=${newPage}`);
+
+        // Create a new URLSearchParams instance to manipulate the query string
+        const params = new URLSearchParams(searchParams.toString());
+
+        // Update the page parameter
+        params.set('page', newPage.toString());
+
+        // Update the URL with the new query string
+        router.push(`/flight/search?${params.toString()}`);
     };
 
     return (
