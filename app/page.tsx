52faf8d0c29b5881ac779945bc1e333ed7fcd8ce
AmirHossein Norbakhsh <amircr7h@icloud.com>
2024-09-10 18:51:22 +0330

    added a button in the homepage to navigate to flight results

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import localFont from 'next/font/local'
+import Link from 'next/link';
+import { Button } from 'primereact/button';
 const IranSans = localFont({ src: '../assets/fonts/IRANSansXFaNum-Regular.ttf' })
 
 export default function Home() {
 
   return (
     <main className={`${IranSans.className} flex flex-row p-6 w-10/12 gap-11 mx-auto mt-12`}>
-    
+    <Link className='mx-auto' href={'/flight/search'}>
+    <Button className='bg-[#ff7913]'>
+      مشاهده  نتایج جستجو
+    </Button>
+    </Link>
     </main>
   );
 }
